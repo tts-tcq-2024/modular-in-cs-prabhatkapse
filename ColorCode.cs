ca104e508564e713dde0a8c4793fade1562bd6f0
prabhatkapse <prabhatkapse@gmail.com>
2024-07-24 15:32:00 +0530

    Update ColorCode.cs

--- ColorCode.cs
+++ ColorCode.cs
@@ -1,13 +1,15 @@
 
+using modular_in_csharp;
+
 namespace TelCo.ColorCoder
 {
-    public class ColorCode : ColorPairs
+    public class ColorCode : ColorMappings
     {
         public ColorPair GetColorFromPairNumber(int pairNumber)
         {
-
             int majorColorsCount = majorColorMapping.Length;
             int minorColorsCount = minorColorMapping.Length;
+
             if (pairNumber < 1 || pairNumber > minorColorsCount * majorColorsCount)
             {
                 throw new ArgumentOutOfRangeException(
@@ -18,17 +20,16 @@ namespace TelCo.ColorCoder
             int majorColorIndex = zeroBasedPairNumber / minorColorsCount;
             int minorColorIndex = zeroBasedPairNumber % minorColorsCount;
 
-            // Construct the return val from the arrays
             ColorPair pair = new ColorPair() { majorColor = majorColorMapping[majorColorIndex], minorColor = majorColorMapping[minorColorIndex] };
 
-            // return the value
             return pair;
         }
 
         public int GetPairNumberFromColor(ColorPair pair)
         {
-            int majorColorIndex = getMajorColorIndex(pair);
-            int minorColorIndex = getMinorClorIndex(pair);
+            var GetColorIndex = new GetColorIndex();
+            int majorColorIndex = GetColorIndex.Major(pair);
+            int minorColorIndex = GetColorIndex.Minor(pair);
 
             if (majorColorIndex == -1 || minorColorIndex == -1)
             {
@@ -37,35 +38,6 @@ namespace TelCo.ColorCoder
             }
 
             return (majorColorIndex * minorColorMapping.Length) + (minorColorIndex + 1);
-        }
-
-        private int getMajorColorIndex(ColorPair pair)
-        {
-
-            for (int i = 0; i < majorColorMapping.Length; i++)
-            {
-                int colorIndex = -1;
-                if (majorColorMapping[i] == pair.majorColor)
-                {
-                    colorIndex = i;
-                    break;
-                }
-            }
-            return colorIndex;
-        } 
-
-        private int getMinorClorIndex(ColorPair pair)
-        {
-            int colorIndex = -1;
-            for (int i = 0; i < minorColorMapping.Length; i++)
-            {
-                if (minorColorMapping[i] == pair.minorColor)
-                {
-                    colorIndex = i;
-                    break;
-                }
-            }
-            return colorIndex;
         }
     }
 }
